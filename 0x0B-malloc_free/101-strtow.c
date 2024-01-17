18541496d3a6e23fcc3239b178a126f4ab74fa1d
BAQLOULZINEB <zinebbaq@gmail.com>
2024-01-16 17:46:48 -0800

    final

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -18,8 +18,7 @@ int count_word(char *s)
 		else if (flag == 0)
 	{
 		flag = 1;
-	        	w++;
-
+		w++;
 	}
 	return (w);
 }
@@ -29,7 +28,7 @@ char **strtow(char *str)
 	int i, k = 0, len = 0, words, c = 0, start, end;
 	while (*(str + len))
 		len++;
-	words = count_word(str);
+		words = count_word(str);
 	if (words == 0)
 		return (NULL);
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
